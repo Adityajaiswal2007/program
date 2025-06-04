16f7ebaa0713ab8ed4c29b8f85e57c20dcc22528
adityajaiswal2007 <avuke143@gmail.com>
2025-06-05 00:32:08 +0530

    sum calc

--- c/test/fuction apna/arrary/p.c
+++ c/test/fuction apna/arrary/p.c
@@ -1,5 +1,25 @@
 #include<stdio.h>
 int main(){
-    
+    int size,sum=0;
+    printf("enter the size of array:");
+    scanf("%d",&size);
+    int arr[size];
+    printf("enter %d elements of array",size);
+    for(int i=0;i<size;i++){
+        scanf("%d",&arr[i]);
+    }
+    printf("elements of array are:");
+    for(int i=0;i<size;i++){
+        printf("%d\n",arr[i]);
+    }
+    for(int i=0;i<size;i++){
+        sum = arr[i]+sum;
+
+
+    }
+    int avg=sum/size;
+    printf("The sum is %d\n",sum);
+    printf("The average is %d",avg);
+
     return 0;
 }
